711384f4727232529f58124eebd3f0e9d15be397
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-03-07 16:00:29 +0100

    Add redirection for incorrect url & add activeclassname

--- src/components/Pagination/PaginationRoute.js
+++ src/components/Pagination/PaginationRoute.js
@@ -1,17 +1,25 @@
-import React from "react";
-import {  Link, useParams } from "react-router-dom";
+import React, {useEffect} from "react";
+import {  NavLink, useParams, useNavigate} from "react-router-dom";
 
 const PaginationRoute = (props) => {
     const {page} = useParams(); 
-    console.log(page,'page')
     const {children, path, limit} = props
     const length = children.length
+    const navigate = useNavigate()
     const begin = limit *(page - 1)
     const end = page * limit
     const pages = Math.ceil(length / limit)
+ 
+    useEffect(()=>{
+        if(page > pages || !isNaN(parseInt(page)) ){
+            navigate(`${path}/1`)
+        }
+    },[navigate, page, pages, path])
+    
+    const activeClass = 'active';
     const links = (new Array(pages).fill(0)).map((item, index) => 
         <li key={index}>
-            <Link to={`${path}/${index +1 }`}>{index + 1 }</Link>
+            <NavLink activeclassname={activeClass} to={`${path}/${index +1 }`}>{index + 1 }</NavLink>
         </li>
     )
 
