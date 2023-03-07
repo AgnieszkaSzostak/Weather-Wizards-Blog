16601a66fb538e0b9d6d8a83bc4b3384f39b9b6a
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-03-07 15:58:29 +0100

    Change component Category to CategoryPosts

---
+++ src/components/CategoryPosts.js
@@ -0,0 +1,40 @@
+import React, {useEffect} from "react";
+import {  usePrismicDocumentByUID, usePrismicDocumentsByType } from '@prismicio/react';
+import { useParams } from 'react-router-dom';
+import PostList from './PostList';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const CategoryPosts = () => {
+    const {name} = useParams()
+
+    const [category, categoryState] = usePrismicDocumentByUID('category',name)
+    const [blogPosts] = usePrismicDocumentsByType("post")
+    const path =useLocation()
+    const navigate = useNavigate()
+    useEffect(() => {
+        if(path.pathname === `/category/${name}`){
+          navigate(`/category/${name}/1`)
+        }
+      
+      }, [navigate, path, name])
+    useEffect(() => {
+        if(categoryState.state === "failed"){
+            navigate('/page404')
+        }
+    }, [navigate, categoryState])
+    if(category && blogPosts){
+        const allPosts = blogPosts.results;
+        const postCat = category.data.posts.map(item => item.post)
+        const filteredPosts = allPosts.filter(post => {
+            return postCat.some(item => item.id === post.id)
+        })
+        return (
+            <div >
+                <PostList path={`/category/${name}`} posts={filteredPosts}/>
+            </div>
+        )
+    }
+
+}
+
+export default CategoryPosts
